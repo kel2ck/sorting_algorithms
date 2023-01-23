c7b60d01e3265c16ad87e5df1e58ebde155d95d2
kelvin onumajuru <kelvinonumajuru@gmail.com>
2023-01-23 04:40:35 -0800

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - perform insertion sort on a linked list
+ * insertion_sort_list - perform insertion sort on a linked list.
  * @list: head node of the linked list
  */
 void insertion_sort_list(listint_t **list)
